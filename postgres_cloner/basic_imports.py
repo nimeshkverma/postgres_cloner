5d71f2dcb2c86f713196133226a28150e56823de
Utsav Tiwary <utsav.tiwary2011@gmail.com>
2015-11-19 00:19:46 +0530

    basic imports removed; minor fixes

--- postgres_cloner/basic_imports.py
+++
@@ -1,5 +0,0 @@
-import logging
-LOGGER = logging.getLogger("search_relevance_app")
-
-logging.basicConfig(filename="db_split.log", level=logging.INFO,
-                    format='%(asctime)s.%(msecs)d %(levelname)s %(module)s - %(funcName)s: %(message)s', datefmt="%Y-%m-%d %H:%M:%S")
